e3ec894323a24982804fba388a3e7421c3dd38d0
James Ngugi <ngugi823@gmail.com>
2022-03-14 14:16:14 +0300

    Add function to prefix country code to phone numbers

---
+++ src/functions.php
@@ -0,0 +1,46 @@
+<?php
+
+/*
+ * @copyright Deadan Group Limited
+ * <code> Build something people want </code>
+ */
+
+/**
+ * Fix phonenumber to start with country code.
+ *
+ * Only tested with african phone numbers.
+ *
+ * @param $number
+ * @param $country_code
+ * @return false|mixed|string
+ */
+function fix_phone($number, $country_code)
+{
+    if (starts_with($number, '+')) {
+        $number = substr($number, 1);
+    } elseif (starts_with($number, '07')) {
+        $number = $country_code.substr($number, 1);
+    } elseif (starts_with($number, '7')) {
+        $number = $country_code.$number;
+    }
+
+    return $number;
+}
+
+if (!function_exists('starts_with')) {
+    /**
+     * Determine if a given string starts with a given substring.
+     *
+     * @param  string  $haystack
+     * @param  string  $needle
+     * @return bool
+     */
+    function starts_with($haystack, $needle)
+    {
+        if ((string) $needle !== '' && strncmp($haystack, $needle, strlen($needle)) === 0) {
+            return true;
+        }
+
+        return false;
+    }
+}
