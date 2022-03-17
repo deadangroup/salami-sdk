29d1603f6da3d7d6e367101aed8e3ada683db94a
James Ngugi <ngugi823@gmail.com>
2022-03-17 03:28:29 +0300

    Add ability to specify app id for payments plugin

--- src/Plugins/SalamiPay.php
+++ src/Plugins/SalamiPay.php
@@ -10,8 +10,6 @@
 
 namespace Deadan\Salami\Plugins;
 
-use Deadan\Salami\Jobs\ProcessSalamiApiResponse;
-
 
 /**
  * Class SalamiPay
@@ -22,81 +20,89 @@ class SalamiPay extends BaseSdk
 {
     /**
      * @param  array  $payload
+     * @param         $appId
      * @return \Deadan\Salami\SalamiApiResponse
      * @throws \Exception
      */
-    public function queryPayments(array $payload = [])
+    public function queryPayments(array $payload = [], $appId)
     {
-        return $this->call("/payments/".$this->getAppId()."/queryPayments", 'GET', $payload);
+        return $this->call("/payments/".$this->getAppId($appId)."/queryPayments", 'GET', $payload);
     }
 
     /**
      * @param $paymentId
+     * @param $appId
      * @return \Deadan\Salami\SalamiApiResponse
      * @throws \Exception
      */
-    public function getPayment($paymentId)
+    public function getPayment($paymentId, $appId)
     {
-        return $this->call("/payments/".$this->getAppId()."/payment/".$paymentId, 'GET');
+        return $this->call("/payments/".$this->getAppId($appId)."/payment/".$paymentId, 'GET');
     }
 
     /**
      * @param  array  $payload
+     * @param         $appId
      * @return \Deadan\Salami\SalamiApiResponse
      * @throws \Exception
      */
-    public function checkBalance(array $payload = [])
+    public function checkBalance(array $payload = [], $appId)
     {
-        return $this->call("/payments/".$this->getAppId()."/checkBalance", 'GET', $payload);
+        return $this->call("/payments/".$this->getAppId($appId)."/checkBalance", 'GET', $payload);
     }
 
     /**
      * @param  array  $payload
+     * @param         $appId
      * @return \Deadan\Salami\SalamiApiResponse
      * @throws \Exception
      */
-    public function extractPayment(array $payload = [])
+    public function extractPayment(array $payload = [], $appId)
     {
-        return $this->call("/payments/".$this->getAppId()."/extractPayment", 'GET', $payload);
+        return $this->call("/payments/".$this->getAppId($appId)."/extractPayment", 'GET', $payload);
     }
 
     /**
      * @param  array  $payload
+     * @param         $appId
      * @return \Deadan\Salami\SalamiApiResponse
      * @throws \Exception
      */
-    public function fetchPayments(array $payload = [])
+    public function fetchPayments(array $payload = [], $appId)
     {
-        return $this->call("/payments/".$this->getAppId()."/fetchPayments", 'GET', $payload);
+        return $this->call("/payments/".$this->getAppId($appId)."/fetchPayments", 'GET', $payload);
     }
 
     /**
      * @param $paymentId
+     * @param $appId
      * @return \Deadan\Salami\SalamiApiResponse
      * @throws \Exception
      */
-    public function getPaymentStatus($paymentId)
+    public function getPaymentStatus($paymentId, $appId)
     {
-        return $this->call("/payments/".$this->getAppId()."/getPaymentStatus/".$paymentId, 'GET');
+        return $this->call("/payments/".$this->getAppId($appId)."/getPaymentStatus/".$paymentId, 'GET');
     }
 
     /**
      * @param  array  $payload
+     * @param         $appId
      * @return \Deadan\Salami\SalamiApiResponse
      * @throws \Exception
      */
-    public function registerUrls(array $payload = [])
+    public function registerUrls(array $payload = [], $appId)
     {
-        return $this->call("/payments/".$this->getAppId()."/registerUrls", 'GET', $payload);
+        return $this->call("/payments/".$this->getAppId($appId)."/registerUrls", 'GET', $payload);
     }
 
     /**
      * @param  array  $payload
+     * @param         $appId
      * @return \Deadan\Salami\SalamiApiResponse
      * @throws \Exception
      */
-    public function requestPayment(array $payload = [])
+    public function requestPayment(array $payload = [], $appId)
     {
-        return $this->call("/payments/".$this->getAppId()."/requestPayment", 'GET', $payload);
+        return $this->call("/payments/".$this->getAppId($appId)."/requestPayment", 'GET', $payload);
     }
 }
